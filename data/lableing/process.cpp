548aeba4bfd53951e4031a328fd4df3fde1258aa
brown0394 <73463402+brown0394@users.noreply.github.com>
2022-07-25 00:11:42 +0900

    clustering

--- data/lableing/process.cpp
+++ data/lableing/process.cpp
@@ -9,6 +9,9 @@
 #define TRAINFRQSTD 10
 #define TRAINAVGSTD 35.9
 
+#define TESTMAXTIME 41122
+#define TRAINMAXTIME 34459
+
 int PostProcess() {
 	std::cout << "Input file name : ";
 	char in_filename[20];
@@ -31,8 +34,7 @@ int PostProcess() {
 	sector_last.SetId("null");
 
 	int count = 0;
-	char line[50];
-	char last_id[50];
+	char line[51];
 
 	while (1) {
 		if (count < 2) {
@@ -92,8 +94,7 @@ int Label(const int frequency_std, const float avg_std) {
 
 	int count = 0;
 	unsigned int label_count = 0;
-	char line[50];
-	char last_id[50];
+	char line[51];
 
 	while (1) {
 		if (count < 3) {
@@ -129,7 +130,108 @@ int Label(const int frequency_std, const float avg_std) {
 	out_file.close();
 }
 
+int timeAvgConvert(const double max_val) {
+	std::cout << "Input file name : ";
+	char in_filename[20];
+	std::cin >> in_filename;
+	std::ifstream in_file(in_filename, std::ios::in);
+	if (in_file.fail()) {
+		std::cout << "File open failed!" << std::endl;
+		return 0;
+	}
+	std::cout << "Output file name : ";
+	char out_filename[20];
+	std::cin >> out_filename;
+	std::ofstream out_file(out_filename, std::ios::out);
+	if (out_file.fail()) {
+		std::cout << "File open failed!" << std::endl;
+		return 0;
+	}
+	Sector sector;
+
+	int count = 0;
+	char line[51];
+
+	while (1) {
+		if (count < 3) {
+			in_file.getline(line, 50, ',');
+			if (in_file.fail()) break;
+			switch (count) {
+			case 0: {sector.SetId(line);  break; }
+			case 1: {
+				sector.SetFrequency(std::stoi(line)); break; }
+			case 2: {
+				if (strcmp(line, "0") == 0) {
+					sector.SetAvg(max_val);
+				}
+				else {
+					sector.SetAvg(std::stod(line));
+				}
+				break;
+			}
+			}
+		}
+		else {
+			in_file.getline(line, 50, '\n');
+			sector.SetSize(std::stol(line));
+			count = -1;
+			out_file << sector << '\n';
+		}
+		++count;
+	}
+	in_file.close();
+	out_file.close();
+}
+
+int clusterCount() {
+	std::cout << "Input file name : ";
+	char in_filename[30];
+	std::cin >> in_filename;
+	std::ifstream in_file(in_filename, std::ios::in);
+	if (in_file.fail()) {
+		std::cout << "File open failed!" << std::endl;
+		return 0;
+	}
+
+	Sector sector;
+
+	int count = 0;
+	char line[51];
+
+	unsigned int num_count = 0;
+	unsigned int marked_count = 0;
+	for (int i = 0; i < 4; ++i) {
+		in_file.getline(line, 50, ',');
+	}
+	in_file.getline(line, 50, '\n');
+	while (1) {
+		if (count < 4) {
+			in_file.getline(line, 50, ',');
+			if (in_file.fail()) 
+				break;
+			switch (count) {
+			case 0: {break; }
+			case 1: {break;  }
+			case 2: {break; }
+			case 3: {break; }
+			}
+		}
+		else {
+			in_file.getline(line, 50, '\n');
+			if (strcmp(line, "0") == 0) {
+				++marked_count;
+			}
+			++num_count;
+			count = -1;
+		}
+		++count;
+	}
+	in_file.close();
+	std::cout << "mark count : " << marked_count << "\nnum count : " << num_count << std::endl;
+}
 int main() {
 	//PostProcess();
-	Label(TRAINFRQSTD, TRAINAVGSTD);
+	//Label(TRAINFRQSTD, TRAINAVGSTD);
+	//timeAvgConvert(TRAINMAXTIME);
+	clusterCount();
 }
